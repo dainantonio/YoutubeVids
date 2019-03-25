cab0cc6a1c3f72075bb59df74ff71ac2095b0e49
dainr <dain.russell@gmail.com>
2019-03-24 23:50:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/youtubevids/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.youtubevids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Vector;
+
+// https://android-coffee.com/tutorial-how-to-play-youtube-videos-in-android-studio-3-1-1/
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Vector<YouTubeVids> youtubeVideos = new Vector<YouTubeVids>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager( new LinearLayoutManager(this));
+
+        youtubeVideos.add( new YouTubeVids("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/eWEF1Zrmdow\" frameborder=\"0\" allowfullscreen></iframe>") );
+        youtubeVideos.add( new YouTubeVids("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/KyJ71G2UxTQ\" frameborder=\"0\" allowfullscreen></iframe>") );
+        youtubeVideos.add( new YouTubeVids("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/y8Rr39jKFKU\" frameborder=\"0\" allowfullscreen></iframe>") );
+        youtubeVideos.add( new YouTubeVids("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/8Hg1tqIwIfI\" frameborder=\"0\" allowfullscreen></iframe>") );
+        youtubeVideos.add( new YouTubeVids("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/uhQ7mh_o_cM\" frameborder=\"0\" allowfullscreen></iframe>") );
+
+        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);
+
+        recyclerView.setAdapter(videoAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/youtubevids/YouTubeVids.java
@@ -0,0 +1,21 @@
+package com.example.youtubevids;
+
+public class YouTubeVids {
+
+    String videoUrl;
+
+    public YouTubeVids() {
+    }
+
+    public YouTubeVids(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
